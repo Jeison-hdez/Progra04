20bdcd841d051af96fb8aea055d356d0de24277d
Marco Espinoza <espinozavega985@gmail.com>
2023-07-24 14:51:55 -0600

    Inicio de interfaz
    Se empezo la interfaz

---
+++ Proyecto_p04/Proyecto_p04/frmVentanilla01.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_p04
+{
+    public partial class frmVentanilla01 : Form
+    {
+        public frmVentanilla01()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVentanilla01_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
