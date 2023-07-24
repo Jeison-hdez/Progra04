c18e7470258188eec026f986aed65d72dc3a0224
ExosData <60944644+exosdata@users.noreply.github.com>
2023-07-24 13:29:38 -0600

    Conexion base de datos y login

---
+++ Proyecto_p04/Proyecto_p04/conexionBD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_p04
+{
+    class conexionBD
+    {
+        public static SqlConnection conectarBD()
+        {
+            SqlConnection cmd = new SqlConnection("SERVER=DESKTOP-MPLP2PA\\SQLEXPRESS;DATABASE=planilla;Integrated security=true");
+            cmd.Open();
+            return cmd;
+        }
+    }
+}
