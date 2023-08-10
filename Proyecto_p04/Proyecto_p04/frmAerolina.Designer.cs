4aed6848af8ee27fd8dd68688e43a5adba29ccf4
AdolCris <139937271+AdolCris@users.noreply.github.com>
2023-08-10 11:23:46 -0600

    11:23

--- Proyecto_p04/Proyecto_p04/frmAerolina.Designer.cs
+++ Proyecto_p04/Proyecto_p04/frmAerolina.Designer.cs
@@ -114,7 +114,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(147, 163);
+            this.label3.Location = new System.Drawing.Point(25, 232);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(117, 20);
             this.label3.TabIndex = 4;
@@ -133,7 +133,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(25, 232);
+            this.label4.Location = new System.Drawing.Point(147, 163);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 20);
             this.label4.TabIndex = 6;
